9109c0f9cbc233b2ac4ddcecf14585a7db341a56
CelestinaProyect <ProyectCelestina@protonmail.com>
2024-06-16 02:22:40 -0500

    Update scr_button-printNload-week.js

--- script.js/scr_button-printNload-week.js
+++ script.js/scr_button-printNload-week.js
@@ -9,7 +9,7 @@
                 if (document.getElementById('statusFinalizado').checked) filters.push('Finalizado');
                 if (document.getElementById('statusEliminado').checked) filters.push('Eliminado');
                 const filteredNovels = novels.filter(novel => filters.includes(novel.status));
-                let output = '**TOP DE NOVELAS MÁS SEGUIDAS PUBLICADAS EN NIADD**\n\n';
+                let output = '**TOP DE NOVELAS MÁS VISITADAS PUBLICADAS EN NIADD**\n\n';
                 filteredNovels.forEach((novel, index) => {
                     output += `- Top ${index + 1}: ${novel.title}\nVisitas: 0000 → ${novel.views}\nSeguidores: 0000 → ${novel.followers}\nCapítulos: 0000 → ${novel.chapters}\n\n${novel.url}\n\n**Ganaste 000 seguidores**\n\n>———◇———<\n\n`;
                 });
